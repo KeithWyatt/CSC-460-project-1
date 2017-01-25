31b054d4b2f744723af30a5be0b9670f7a1788e9
Becky <bdicroteau@gmail.com>
2017-01-24 21:25:43 -0800

    included arduino sketch directory structure, added code to read value from light sensor - not fully working, joystick interference.

--- joy___i2c/joy___i2c.ino
+++ joy___i2c/joy___i2c.ino
@@ -1,18 +1,10 @@
-/* YourDuino.com Example Software Sketch
- 16 character 2 line I2C Display
- Backpack Interface labelled "A0 A1 A2" at lower right.
- ..and
- Backpack Interface labelled "YwRobot Arduino LCM1602 IIC V1"
- MOST use address 0x27, a FEW use 0x3F
- terry@yourduino.com */
+
 
 /*-----( Import needed libraries )-----*/
 #include <Wire.h>  // Comes with Arduino IDE
-// Get the LCD I2C Library here: 
-// https://bitbucket.org/fmalpartida/new-liquidcrystal/downloads
-// Move any other LCD libraries to another folder or delete them
-// See Library "Docs" folder for possible commands etc.
+
 #include <LiquidCrystal_I2C.h>
+#include <Servo.h> 
 
 /*-----( Declare Constants )-----*/
 /*-----( Declare objects )-----*/
@@ -23,27 +15,40 @@
 LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
 
 /*-----( Declare Variables )-----*/
-//NONE
 
 const int analogXInPin = A0;  // Analog X input pin that the potentiometer is attached to
 const int analogXOutPin = 9; // Analog X output pin that the LED is attached to
-const int analogYInPin = A1;
-const int analogYOutPin = 10;
-const int analogLInPin = A3;
+const int analogYInPin = A1; // Analog Y input pin
+const int analogYOutPin = 10;  // Analog Y output pin
+const int analogLInPin = A3;  // Analog pin for light sensor
+
 
 int sensorXValue = 0;        // value read from the pot
 int outputXValue = 0;        // value output to the PWM (analog out)
+
 int sensorYValue = 0;
 int outputYValue = 0;
-int oldXValue = 10;
-int oldYValue = 10;
 
-int buttonPin = 19;
+int oldXValue = 10;        // can maybe change?
+int oldYValue = 10;        // can maybe change?
+
+int buttonPin = 19;        // uses pin 19 for joystick button
 int buttonState = 0;
-int oldButtonState = 1;
+int oldButtonState = 1;    // opposite of buttonState so the application can check button changes
 
 int lightValue = 0;
 int outputLightValue = 0;
+
+Servo myservo;  // create servo object to control a servo 
+
+//int defaultpos = 1500;    // variable to store the servo position 
+int lowpos = 1000;        // lowest servo position
+int highpos = 2000;      // highest servo position
+int currentX = 0;          // current X servo position
+int currentY = 0;          // current Y servo position
+int oldCurrentXPos = 1500;    // old current X servo position
+int oldCurrentYPos = 1500;    // old current Y servo position
+
 
 
 void setup()   /*----( SETUP: RUNS ONCE )----*/
@@ -53,6 +58,10 @@ void setup()   /*----( SETUP: RUNS ONCE )----*/
   lcd.begin(16,2);   // initialize the lcd for 16 chars 2 lines, turn on backlight
   
   pinMode(buttonPin, INPUT_PULLUP); //for joystick button
+  
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
+  myservo.writeMicroseconds(1500);  // set servo to mid-point
+
 
 }
 void loop()   /*----( LOOP: RUNS CONSTANTLY )----*/
@@ -73,19 +82,14 @@ void loop()   /*----( LOOP: RUNS CONSTANTLY )----*/
   outputYValue = map(sensorYValue, 0, 1023, 0, 255);  
   // change the analog out value:
   analogWrite(analogYOutPin, outputYValue);  
-
- //read the analog in value for light sensor
- lightValue = analogRead(analogLInPin);
- outputLightValue = map(lightValue,0,1023,0,255);
+ 
 
   buttonState = digitalRead(buttonPin);  
   
   if( (oldXValue > (sensorXValue + 25)) || (oldXValue < (sensorXValue - 25)) || 
   (oldYValue > (sensorYValue + 25)) || (oldYValue < (sensorYValue - 25)) || 
-  (oldButtonState != buttonState)){
+  (oldButtonState != buttonState || oldYValue < 30 || oldYValue > 990 || oldXValue <30 || oldXValue > 990)){
   
-  oldXValue = sensorXValue;
-  oldYValue = sensorYValue; 
 
   // print the results to the serial monitor:
   Serial.print("X: " );                       
@@ -101,7 +105,11 @@ void loop()   /*----( LOOP: RUNS CONSTANTLY )----*/
 */
   Serial.print(" | B: ");
   Serial.println(buttonState);
-  Serial.print(outputLightValue);                 
+
+
+  //read the analog in value for light sensor
+  lightValue = analogRead(analogLInPin);
+  outputLightValue = map(lightValue,0,1023,0,255);
 
   lcd.setCursor(0,0);
   lcd.print("X:");
@@ -123,9 +131,45 @@ void loop()   /*----( LOOP: RUNS CONSTANTLY )----*/
     Serial.print("off");
     lcd.print("OFF");
   }
+
+/*
+  if( sensorXValue < 497){
+    for(currentX = oldCurrentXPos; currentX >= (sensorXValue + 1000); currentX -= 1)     // moves servo X position to the left
+  {                                
+    myservo.writeMicroseconds(currentX);              // tell servo to go to position in variable 'currentX' 
+    delayMicroseconds(2500);                       // waits 2.5ms for the servo to reach the position 
+  }   
+  }
+ 
+  if( sensorXValue > 497){
+    for(currentX = oldCurrentXPos; currentX <= (sensorXValue + 1000); currentX += 1)     // moves servo X position to the right
+  {                                
+    myservo.writeMicroseconds(currentX);              // tell servo to go to position in variable 'currentX' 
+    delayMicroseconds(2500);                       // waits 2.5ms for the servo to reach the position 
+  }   
+  }
   
+/*  if( sensorYValue < 508){
+    for(currentY = oldCurrentYPos; currentY >= (sensorYValue + 1000); currentY -= 1)     // moves servo Y position to the down
+  {                                
+    myservo.writeMicroseconds(currentY);              // tell servo to go to position in variable 'currentY' 
+    delayMicroseconds(2500);                       // waits 2.5ms for the servo to reach the position 
+  }   
+  }
   
+  if( sensorYValue > 508){
+    for(currentY = oldCurrentYPos; currentY <= (sensorYValue + 1000); currentY += 1)     // moves servo X position to the right
+  {                                
+    myservo.writeMicroseconds(currentY);              // tell servo to go to position in variable 'currentY' 
+    delayMicroseconds(2500);                       // waits 2.5ms for the servo to reach the position 
+  }   
+  }
+ */
+  oldXValue = sensorXValue;
+  oldYValue = sensorYValue;
   
+  oldCurrentXPos = currentX;
+  oldCurrentYPos = currentY;
   
   delay(2);
   }
